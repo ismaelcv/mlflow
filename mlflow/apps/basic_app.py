87d831276e8a4c0cb9e1aca5a5693d8a4e41bc53
ismaelcv <ismaelcv@gmail.com>
2022-09-20 14:09:25 +0200

    docker image working

--- mlflow/apps/basic_app.py
+++ mlflow/apps/basic_app.py
@@ -8,6 +8,7 @@ xs = list(range(30))
 ys = [10000 * 1.07**i for i in xs]
 fig = go.Figure(data=go.Scatter(x=xs, y=ys))
 fig.update_layout(xaxis_title="Years", yaxis_title="$")
+
 app.layout = html.Div(children=[html.H1(children="Assets"), dcc.Graph(figure=fig)])
 
 if __name__ == "__main__":
